7f8e16541fe0e8dc54934967ddd0b5538ebf2038
Elihu Garrett <pmaekb@hotmail.com>
2017-04-03 13:24:12 -0500

    Update skecth0.lua

--- skecth0.lua
+++ skecth0.lua
@@ -8,7 +8,7 @@ local f2 = function(p) return p:add(vec2(0, 0.2)):rotate(-0.4):mul(0.3) end
 local f3 = function(p) return p:add(vec2(0.2, 0.4)):rotate(0.09):mul(0.6) end
 local f4 = function(p) return p:add(vec2(0.2, 0.2)):rotate(0.7):mul(0.3) end
 
-local transforms = { f1, f2, f3, f4 }
+local transforms = {f1, f2, f3, f4}
 
 function plot()
 	for i = 1, 1000 do
